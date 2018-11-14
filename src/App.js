35484e83252f6960bc649b8d0dfdb63ff4fa2fd9
Chris Terrazas <chris.terrazas@gmail.com>
2018-11-14 16:31:24 -0500

    initial commit - color sider works with alpha

--- src/App.js
+++ src/App.js
@@ -1,28 +1,84 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      h: 0,
+      s: 0,
+      l: 0,
+      a: 0
+    }
+  }
+
+  slideH = event => {
+    this.setState({
+      h: event.target.value
+    })
+  }
+  slideS = event => {
+    this.setState({
+      s: event.target.value
+    })
+  }
+  slideL = event => {
+    this.setState({
+      l: event.target.value
+    })
+  }
+  slideA = event => {
+    this.setState({
+      a: event.target.value
+    })
+  }
+
   render() {
+    const color = `hsla(${this.state.h}, ${this.state.s}%, ${this.state.l}%, ${
+      this.state.a
+    })`
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="container">
+        <main>
+          <p>{color}</p>
+          <div id="color-box" style={{ background: color }} />
+          <label>Hue: {this.state.h}</label>
+          <input
+            type="range"
+            min="0"
+            max="360"
+            value={this.state.h}
+            onChange={this.slideH}
+          />
+          <label>Saturation: {this.state.s}</label>
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={this.state.s}
+            onChange={this.slideS}
+          />
+          <label>Lightness: {this.state.l}</label>
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={this.state.l}
+            onChange={this.slideL}
+          />
+          <label>Alpha: {this.state.a}</label>
+          <input
+            type="range"
+            min="0"
+            max="1"
+            step=".1"
+            value={this.state.a}
+            onChange={this.slideA}
+          />
+        </main>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
